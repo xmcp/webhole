a7d548fbb95d20dc0aa610bf8ad98a14b66b88a9
xmcp <xiaoyuanan2000@sina.com>
2018-08-20 22:05:59 +0800

    update ui

--- src/Title.js
+++ src/Title.js
@@ -7,22 +7,18 @@ export function Title(props) {
             <div className="title-links">
                 <a onClick={()=>{props.callback(
                     '关于 P大树洞（非官方） 网页版',
-                    <div>
-                        <br />
+                    <div class="box">
                         <p>使用提示：</p>
                         <ul>
-                            <li>为保证使用体验，请使用分辨率恰好为 1920*1080 像素的电脑，并用 Chrome 浏览器 stable 分支最新版</li>
+                            <li>为保证使用体验，请使用 Chrome 浏览器 stable 分支最新版</li>
                             <li>在列表中点击帖子可以显示全部回复</li>
-                            <li>搜索帖子功能正在开发中</li>
                         </ul>
-                        <br />
                         <p>使用本网站时，您需要了解并同意：</p>
                         <ul>
                             <li>所有数据来自 PKU Helper，本站不对其内容负责</li>
                             <li>不接受关于修改 UI 的建议</li>
                             <li>英梨梨是我的，你们都不要抢</li>
                         </ul>
-                        <br />
                         <p>By @xmcp</p>
                         <br />
                         <p>
@@ -31,6 +27,7 @@ export function Title(props) {
                             the Free Software Foundation, either version 3 of the License, or
                             (at your option) any later version.
                         </p>
+                        <br />
                         <p>
                             This program is distributed in the hope that it will be useful,
                             but WITHOUT ANY WARRANTY; without even the implied warranty of
