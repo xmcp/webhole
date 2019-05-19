5f61e189e695d2b9ccc85b1f51643b6b9fb83520
xmcp <xiaoyuanan2000@sina.com>
2019-05-19 23:15:11 +0800

    Redo "update sidebar style" (fix ios safari)

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -6,11 +6,11 @@ export function Sidebar(props) {
         <div className={props.content ? 'sidebar-on' : ''}>
             <div className="sidebar-shadow" onClick={props.do_close} />
             <div className="sidebar">
-                <p className="sidebar-title">
-                    <a onClick={props.do_close}>&nbsp;<span className="icon icon-back" />&nbsp;</a>
-                    {props.title}
-                </p>
                 {props.content}
+            </div>
+            <div className="sidebar-title">
+                <a onClick={props.do_close}>&nbsp;<span className="icon icon-back" />&nbsp;</a>
+                {props.title}
             </div>
         </div>
     );
