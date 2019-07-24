f513ccf14b4ee17a54029e1738d456d6bcb38c5d
xmcp <xiaoyuanan2000@sina.com>
2019-07-24 23:17:59 +0800

    revert kyoani easter egg

--- src/text_splitter.js
+++ src/text_splitter.js
@@ -1,14 +1,10 @@
 export const PID_RE=/(^|[^\d])([1-9]\d{4,5})(?!\d|\u20e3|\ufe0e|\ufe0f)/g;
 export const NICKNAME_RE=/(^|[^A-Za-z])((?:(?:Angry|Baby|Crazy|Diligent|Excited|Fat|Greedy|Hungry|Interesting|Japanese|Kind|Little|Magic|Naïve|Old|Powerful|Quiet|Rich|Superman|THU|Undefined|Valuable|Wifeless|Xiangbuchulai|Young|Zombie)\s)?(?:Alice|Bob|Carol|Dave|Eve|Francis|Grace|Hans|Isabella|Jason|Kate|Louis|Margaret|Nathan|Olivia|Paul|Queen|Richard|Susan|Thomas|Uma|Vivian|Winnie|Xander|Yasmine|Zach)|You Win(?: \d+)?|洞主)(?![A-Za-z])/gi;
 export const URL_RE=/(^|[^.@a-zA-Z0-9_])((?:https?:\/\/)?(?:(?:[\w-]+\.)+[a-zA-Z]{2,3}|\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})(?::\d{1,5})?(?:\/[\w~!@#$%^&*()\-_=+[\];,./?]*)?)(?![a-zA-Z0-9])/gi;
-export const EASTER_EGG_KYOANI_RE=/(京都动画|京阿尼|京アニ)/g;
 
 export function split_text(txt,rules) {
     // rules: [['name',/regex/],...]
     // return: [['name','part'],[null,'part'],...]
-
-    if(window.config.easter_egg && (+new Date())<=1563548400000) // Jul 19 2019 23:00:00 GMT+0800
-        rules=[...rules,['easter_egg_kyoani',EASTER_EGG_KYOANI_RE]];
 
     txt=[[null,txt]];
     rules.forEach((rule)=>{
