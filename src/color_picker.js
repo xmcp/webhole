ce4a867143f3295971b6795200d091d0fcb5aae0
xmcp <xiaoyuanan2000@sina.com>
2020-03-26 20:30:28 +0800

    update style in dark mode

--- src/color_picker.js
+++ src/color_picker.js
@@ -11,14 +11,14 @@ export class ColorPicker {
     get(name) {
         name=name.toLowerCase();
         if(name==='洞主')
-            return ['hsl(0,0%,97%)','hsl(0,0%,25%)'];
+            return ['hsl(0,0%,97%)','hsl(0,0%,16%)'];
 
         if(!this.names[name]) {
             this.current_h+=golden_ratio_conjugate;
             this.current_h%=1;
             this.names[name]=[
                 `hsl(${this.current_h*360}, 50%, 90%)`,
-                `hsl(${this.current_h*360}, 60%, 25%)`,
+                `hsl(${this.current_h*360}, 60%, 20%)`,
             ];
         }
         return this.names[name];
